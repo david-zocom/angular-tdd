13c18160b1b15bd0e37a594b6334b8704008588e
David Andersson <David Andersson>
2019-04-26 11:12:30 +0200

    Exercise 4, complete

--- src/app/exercise/exercise.service.spec.ts
+++ src/app/exercise/exercise.service.spec.ts
@@ -3,12 +3,53 @@ import { TestBed } from '@angular/core/testing';
 import { ExerciseService } from './exercise.service';
 
 describe('ExerciseService', () => {
-	beforeEach(() => TestBed.configureTestingModule({}));
+	let service: ExerciseService;
+	beforeEach(() => {
+		TestBed.configureTestingModule({})
+		service = TestBed.get(ExerciseService);
+	});
 
 	it('should be created', () => {
-		const service: ExerciseService = TestBed.get(ExerciseService);
 		expect(service).toBeTruthy();
 	});
 
+	// returnera true om det är ett tal mellan 1 och 10
+	// annars false
+	it('should return true for finger number', () => {
+		let num = 1.337;  // arrange
+		let actual = service.isFingerNumber(num); // act
+		expect(actual).toBe(true);
+	})
 
+	it('should return true for 1 and 10', () => {
+		let low = 1, high = 10;
+		expect(service.isFingerNumber(low)).toBe(true);
+		expect(service.isFingerNumber(high)).toBe(true);
+	})
+
+	const epsilon = 0.0001;
+	it('should return false below the border', () => {
+		let low = 1 - epsilon;
+		expect(service.isFingerNumber(low)).toBe(false);
+	})
+	it('should return false above the border', () => {
+		let high = 10 + epsilon;
+		expect(service.isFingerNumber(high)).toBe(false);
+	})
+
+	it('should return false if not a number', () => {
+		expect(service.isFingerNumber('1')).toBe(false);
+		expect(service.isFingerNumber(null)).toBe(false);
+		expect(service.isFingerNumber(undefined)).toBe(false);
+	})
+	it('should return false if NaN', () => {
+		expect(service.isFingerNumber(NaN)).toBe(false);
+	})
 });
+
+
+
+
+
+
+//
--- src/app/exercise/exercise.service.ts
+++ src/app/exercise/exercise.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class ExerciseService {
 
 	constructor() { }
+
+	isFingerNumber(n: any): boolean {
+		if( typeof n !== 'number' ) return false;
+		else if( isNaN(n) ) return false;
+		else if( n < 1 || n > 10 ) return false;
+		return true;
+	}
 }
+
+/*
+4a Skriv minst tre testfall för funktionen isFingerNumber(n: any): boolean. Funktionen ska returnera true om n är ett tal mellan 1 och 10, annars false.
+Man kan göra testfallen "should return true for n=1", "should return true for n=2" osv., men det blir inte särskilt intressant. Det är bättre att behandla "tal mellan 1 och 10" som ett testfall.
+4b* Implementera funktionen.
+
+*/
